e0f222ae50cad0658454cdb9fe2a59e4a17e7141
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2025-01-23 13:24:20 -0300

    checkpoint

--- pages/4_Educação_x_Partido_de_Voto.py
+++ pages/4_Educação_x_Partido_de_Voto.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-import pandas as pd
+import plotly.figure_factory as ff
 
 from dataset.get_dataset import get_dataframe
 
@@ -12,18 +12,20 @@ st.set_page_config(
 
 df = get_dataframe()
 
+# Título
 st.title("Existe correlação entre níveis de educação e o partido de voto por estado?")
 
-st.write(
-    "Um gráfico de dispersão mostrando a relação entre o nível de educação e a porcentagem de votos por partido em cada estado."
-)
+# Descrição
+'''
+Os gráficos abaixo buscam responder a esta pergunta. Para isso, vamos utilizar alguns métodos distintos de renderização, começando pelo gráfico de dispersão.
+'''
 
 education_level_options = {
     'Population with less than 9th grade education': 'Até a 9ª série',
-    'Population with 9th to 12th grade education, no diploma': '9 à 12ª série (sem diploma)',
+    'Population with 9th to 12th grade education, no diploma': '9ª a 12ª série, sem diploma',
     'High School graduate and equivalent': 'Ensino Médio completo',
     'Some College,No Degree': 'Ensino Superior incompleto',
-    'Associates Degree': 'Diploma de Associado',
+    'Associates Degree': 'Associates Degree',
     'Bachelors Degree': 'Bacharelado',
     'Graduate or professional degree': 'Pós-graduação',
 }
@@ -31,42 +33,46 @@ education_level_options = {
 education_level = st.selectbox(
     'Nível de educação',
     education_level_options.keys(),
+    index=list(education_level_options.keys()).index('Bachelors Degree'),
     format_func=lambda x: education_level_options[x],
 )
 
 df['republican_votes'] = df['2020 Republican vote %'] * df['Total Population']
 df['democrat_votes'] = df['2020 Democrat vote %'] * df['Total Population']
 
+# Gráfico de dispersão para votos republicanos
 fig1 = px.scatter(
     df,
     x=education_level,
-    y='republican_votes',
+    y='2020 Republican vote %',
     color='state',
     size='Total Population',
     hover_name='county',
     size_max=60,
-    title=f'Relação entre a porcentagem de votos republicanos e o nível de educação ({education_level_options[education_level]})',
+    title=f'Porcentagem de votos republicanos pela porcentagem da população no nível {education_level_options[education_level]}',
     labels={
-        education_level: 'Nível de educação',
-        'republican_votes': 'Número de votos republicanos',
+        education_level: f'Porcentagem da população com nível de educação {education_level_options[education_level]}',
+        '2020 Republican vote %': 'Porcentagem de votos republicanos',
     },
 )
 
+# Gráfico de dispersão para votos democratas
 fig2 = px.scatter(
     df,
     x=education_level,
-    y='democrat_votes',
+    y='2020 Democrat vote %',
     color='state',
     size='Total Population',
     hover_name='county',
     size_max=60,
-    title=f'Relação entre a porcentagem de votos democratas e o nível de educação ({education_level_options[education_level]})',
+    title=f'Porcentagem de votos democratas pela porcentagem da população no nível {education_level_options[education_level]}',
     labels={
-        education_level: 'Nível de educação',
-        'democrat_votes': 'Número de votos democratas',
+        education_level: f'Porcentagem da população com nível de educação {education_level_options[education_level]}',
+        '2020 Democrat vote %': 'Porcentagem de votos democratas',
     },
 )
 
+# Encontrando o valor máximo do eixo y em ambos os gráficos
 max_y_value = 0
 for fig in [fig1, fig2]:
     for trace in fig.data:
@@ -76,6 +82,47 @@ for fig in [fig1, fig2]:
 fig1.update_yaxes(range=[0, max_y_value * 1.1])
 fig2.update_yaxes(range=[0, max_y_value * 1.1])
 
-# Exibindo os gráficos
+# Exibindo os gráficos de dispersão
 st.plotly_chart(fig1)
 st.plotly_chart(fig2)
+
+
+st.write("## Matriz de Correlação")
+'''
+Esta matriz de correlação mostra a correlação entre cada nível de educação e a porcentagem de votos para os partidos Republicano e Democrata.
+
+- **Valores mais altos** indicam uma **correlação positiva**.
+- **Valores mais baixos** indicam uma **correlação negativa**.
+- **Valores mais próximos de 0** indicam uma **correlação fraca**.
+'''
+
+correlation_data = df[list(education_level_options.keys()) + ['2020 Republican vote %', '2020 Democrat vote %']]
+correlation_matrix = correlation_data.corr()
+education_party_correlations = correlation_matrix.loc[list(education_level_options.keys()), ['2020 Republican vote %', '2020 Democrat vote %']]
+
+# Define o intervalo da escala de cores como -1 a 1
+fig3 = ff.create_annotated_heatmap(
+    z=education_party_correlations.values,
+    x=['Votos Republicanos', 'Votos Democratas'],
+    y=list(education_level_options.values()),
+    colorscale='inferno',
+    showscale=True,
+    xgap=3,
+    ygap=3,
+    zmin=-1,  # Define o valor mínimo da escala de cores
+    zmax=1   # Define o valor máximo da escala de cores
+)
+
+fig3.update_layout(
+    title_text='Correlação entre Nível de Educação e Partido do Voto',
+    title_x=0.5,
+    xaxis_showgrid=False,
+    yaxis_showgrid=False,
+    xaxis_zeroline=False,
+    yaxis_zeroline=False,
+    yaxis_autorange='reversed',
+    template='plotly_dark'
+)
+
+# Exibindo o mapa de calor
+st.plotly_chart(fig3)
